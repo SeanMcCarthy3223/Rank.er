413be464d2c36d775c9e04533c8b9a41aca37fe5
Sean McCarthy <seanmccarthy3223@GMAIL.com>
2019-09-25 19:15:16 -0500

    Setup basic server and configure DB connection to MySQl

--- db/index.js
+++ db/index.js
@@ -1,13 +1,11 @@
 const mysql = require('mysql')
-const mysqlPass = require('../mysql.config');
+const mysqlPass = require('../mysql.config.js');
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: `${mysqlPass}`,
-  database: 'my_db'
-})
-
-connection.connect()
+  database: 'rankermvp'
+});
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./db/index');
+const bodyParser = require('body-parser');
+const util = require('util');
 
 app.use(express.static('dist'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+db.connect((err) => {
+    if(err) console.error(err);
+
+    console.log('MySQL is connected');
+});
+
+app.get('/allEmployees', (req, res) => {
+    db.query('SELECT employees.id, employees.first_name, employees.last_name, employees.photo, companies.name, employees.up_votes, employees.total_votes FROM employees, companies WHERE employees.company_id = companies.id;',
+    (err, rows, fields) => {
+        if(err) res.status(404).send(err);
+
+        console.log(rows[28]);        
+        res.send(rows);
+    });
+});
 
 app.listen(port, () => console.log(`listening from port: ${port}`));
